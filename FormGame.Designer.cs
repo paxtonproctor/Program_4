5f6bce0d0ab02ceeae282d9221ef192a8a741faa
Paxton Proctor <pax.proctor@gmail.com>
2022-09-20 19:55:54 -0500

    Add project files.

---
+++ FormGame.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Program_4
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
+            this.groupBoxGame = new System.Windows.Forms.GroupBox();
+            this.groupBoxMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(6, 21);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(142, 43);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Location = new System.Drawing.Point(215, 22);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(141, 42);
+            this.buttonSettings.TabIndex = 1;
+            this.buttonSettings.Text = "Settings";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(424, 22);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(141, 42);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxMenu
+            // 
+            this.groupBoxMenu.Controls.Add(this.buttonStart);
+            this.groupBoxMenu.Controls.Add(this.buttonClose);
+            this.groupBoxMenu.Controls.Add(this.buttonSettings);
+            this.groupBoxMenu.Location = new System.Drawing.Point(12, 246);
+            this.groupBoxMenu.Name = "groupBoxMenu";
+            this.groupBoxMenu.Size = new System.Drawing.Size(571, 74);
+            this.groupBoxMenu.TabIndex = 3;
+            this.groupBoxMenu.TabStop = false;
+            this.groupBoxMenu.Text = "Menu";
+            // 
+            // groupBoxGame
+            // 
+            this.groupBoxGame.Location = new System.Drawing.Point(12, 13);
+            this.groupBoxGame.Name = "groupBoxGame";
+            this.groupBoxGame.Size = new System.Drawing.Size(571, 227);
+            this.groupBoxGame.TabIndex = 4;
+            this.groupBoxGame.TabStop = false;
+            this.groupBoxGame.Text = "Game";
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 332);
+            this.Controls.Add(this.groupBoxGame);
+            this.Controls.Add(this.groupBoxMenu);
+            this.Name = "FormGame";
+            this.Text = "Treasure Island";
+            this.groupBoxMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.GroupBox groupBoxMenu;
+        private System.Windows.Forms.GroupBox groupBoxGame;
+    }
+}
+
---
+++ FormGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_4
+{
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+
+            // buttons
+            buttonClose.Click += ButtonClose_Click;
+            buttonSettings.Click += ButtonSettings_Click;
+            buttonStart.Click += ButtonStart_Click;
+        }
+
+        #region button Events
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonSettings_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
